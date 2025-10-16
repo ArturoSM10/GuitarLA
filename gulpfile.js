73d0a0b6c883502593d76093aba812c71bbcf202
Arturo Mendoza <jarsmdza@gmail.com>
2025-10-15 22:37:26 -0600

    Se crearon archivos necesarios para el proyecto

--- gulpfile.js
+++ gulpfile.js
@@ -7,38 +7,36 @@ const cssnano = require('cssnano');
 const imagemin = require('gulp-imagemin');
 const webp = require('gulp-webp');
 const avif = require('gulp-avif');
-const { imagenes } = require('../DeliveryApp/gulpfile');
-
 
 function css(done) {
-    src('./scr/scss/app.scss', {sourcemaps: true})
-    .pipe(sass({outputStyle: 'expanded'}))
-    .pipe(postcss([autoprefixer(), cssnano()]))
-    .pipe(dest('./build/css', {sourcemaps: '.'}));
+    src('./src/scss/app.scss', { sourcemaps: true})
+        .pipe(sass({outputStyle: 'expanded'}))
+        .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(dest('./build/css', {sourcemaps: '.'}));
     done();
 }
 
-function dev(done) {
+function dev() {
     watch('./src/scss/**/*.scss', css);
-    watch('./src/img/**/*', imagenes);
+    watch('./src/img/**/*', imagenes)
 }
 
 function imagenes() {
     return src('./src/img/**/*')
     .pipe(imagemin({optimizationLevel: 3}))
-    .pipe('./build/img');
+    .pipe(dest('./build/img'));
 }
 
 function versionWebp() {
     return src('./src/img/**/*.{jpg, png}')
     .pipe(webp({quality: 50}))
-    .pipe('./build/img');
+    .pipe(dest('./build/img'));
 }
 
 function versionAvif() {
     return src('./src/img/**/*.{jpg, png}')
     .pipe(avif({quality: 50}))
-    .pipe('./build/img');
+    .pipe(dest('./build/img'));
 }
 
 
